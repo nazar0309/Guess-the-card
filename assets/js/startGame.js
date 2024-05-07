12400315b25363217429ecee9f449c6a06d4e023
Nazar Tsuper <nazartsuper0309@gmail.com>
2024-05-07 18:27:52 +0100

    Add fuctions to Create and Shuffle icons

--- assets/js/startGame.js
+++ assets/js/startGame.js
@@ -1,8 +1,14 @@
+import { createIconsArray } from "./utils.js";
+import { dublicateArray } from "./utils.js";
+import { shuffle } from "./utils.js";
 export const startGame = (difficult) => {
     let firstCard = null;
     let secondCard = null;
     let clickable = true;
     const gameSection = document.querySelector('.game-section_container');
     const gameTable = document.createElement('div');
-    
+    const cardsIcons = createIconsArray(difficult);
+    const dublicateCardsIcons = dublicateArray(cardsIcons);
+    shuffle(dublicateCardsIcons);
+    console.log(dublicateCardsIcons);
 }
